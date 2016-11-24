9296f11ab357c031809681772fb0ab171ee9ee31
betterzfz <betterzfz@163.com>
2016-11-24 14:30:49 +0800

    add demo for countdown function

--- library/common-functions.js
+++ library/common-functions.js
@@ -6,19 +6,31 @@
 /**
  * a countdown function
  * @param countdown_time the seconds to countdown
- * @param day_id the id of days element for countdown
- * @param hour_id the id of hours element for countdown
- * @param minute_id the id of minutes element for countdown
  * @param second_id the id of seconds element for countdown
+ * @param minute_id the id of minutes element for countdown
+ * @param hour_id the id of hours element for countdown
+ * @param day_id the id of days element for countdown
  * @stone
  */
-const countdown = (countdown_time, day_id, hour_id, minute_id, second_id) => {
-    return setInterval(countdown_time => {
+function countdown (countdown_time, second_id, minute_id, hour_id, day_id) {
+    return setInterval(function () {
         if (countdown_time > 0) {
-            $('#' + day_id).text(Math.floor(countdown_time / 86400));
-            $('#' + hour_id).text(Math.floor(countdown_time / 3600 - days * 24));
-            $('#' + minute_id).text(Math.floor(countdown_time / 60 - days * 1440 - hours * 60));
-            $('#' + second_id).text(Math.floor(countdown_time) - days * 86400 - hours * 3600 - minutes * 60);
+            const days = Math.floor(countdown_time / 86400);
+            const hours = Math.floor(countdown_time / 3600 - days * 24);
+            const minutes = Math.floor(countdown_time / 60 - days * 1440 - hours * 60);
+            if (second_id) {
+                document.getElementById(second_id).innerHTML = Math.floor(countdown_time) - days * 86400 - hours * 3600 - minutes * 60;
+            }
+            if (minute_id) {
+                document.getElementById(minute_id).innerHTML = minutes;
+            }
+            if (hour_id) {
+                document.getElementById(hour_id).innerHTML = hours;
+            }
+            if (day_id) {
+                document.getElementById(day_id).innerHTML = days;
+            }
+            countdown_time--;
         }
     }, 1000);
 }

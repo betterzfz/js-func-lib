ca5d3b8be00eac4d4ddbe27604de0a3765afad40
betterzfz <betterzfz@163.com>
2016-11-25 17:43:28 +0800

    split the countdown function into two functions named separated_countdown and joined_countdown

--- library/common-functions.js
+++ library/common-functions.js
@@ -4,16 +4,16 @@
  */
 
 /**
- * a countdown function
+ * a separated countdown function
  * @param countdown_time the seconds to countdown
- * @param second_id the id of seconds element for countdown
- * @param minute_id the id of minutes element for countdown
- * @param hour_id the id of hours element for countdown
- * @param day_id the id of days element for countdown
+ * @param second_id the id of the seconds element for countdown
+ * @param minute_id the id of ths minutes element for countdown
+ * @param hour_id the id of the hours element for countdown
+ * @param day_id the id of the days element for countdown
  * @stone
  */
-function countdown (countdown_time, second_id, minute_id, hour_id, day_id) {
-    return setInterval(function () {
+const separated_countdown = (countdown_time, second_id, minute_id, hour_id, day_id) => {
+    return setInterval(() => {
         if (countdown_time > 0) {
             const days = Math.floor(countdown_time / 86400);
             const hours = Math.floor(countdown_time / 3600 - days * 24);
@@ -30,6 +30,60 @@ function countdown (countdown_time, second_id, minute_id, hour_id, day_id) {
             if (day_id) {
                 document.getElementById(day_id).innerHTML = days;
             }
+            countdown_time--;
+        }
+    }, 1000);
+}
+
+/**
+ * a joined countdown function
+ * @param countdown_time the seconds to countdown
+ * @param countdown_id the id of the element for countdown
+ * @param language the language for countdown to show
+ * @stone
+ */
+const joined_countdown = (countdown_time, countdown_id, language) => {
+    return setInterval(() => {
+        if (countdown_time > 0) {
+            const days = Math.floor(countdown_time / 86400);
+            const hours = Math.floor(countdown_time / 3600 - days * 24);
+            const minutes = Math.floor(countdown_time / 60 - days * 1440 - hours * 60);
+            const seconds = Math.floor(countdown_time) - days * 86400 - hours * 3600 - minutes * 60;
+
+            let day_label = ':';
+            let hour_label = ':';
+            let minute_label = ':';
+            let second_label = '';
+            switch (language) {
+                case 'en':
+                    day_label = 'days';
+                    hour_label = 'hours';
+                    minute_label = 'minutes';
+                    second_label = 'seconds';
+                    break;
+                case 'zh-cn':
+                    day_label = '天';
+                    hour_label = '时';
+                    minute_label = '分';
+                    second_label = '秒';
+                    break;
+            }
+
+            let countdown_info = '';
+            if (days > 0) {
+                countdown_info += days + day_label;
+            }
+            if (hours > 0) {
+                countdown_info += hours + hour_label;
+            }
+            if (minutes > 0) {
+                countdown_info += minutes + minute_label;
+            }
+            if (seconds > 0) {
+                countdown_info += seconds + second_label;
+            }
+            document.getElementById(countdown_id).innerHTML = countdown_info;
+
             countdown_time--;
         }
     }, 1000);
